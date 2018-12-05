4c89da22890349eef1baad6328beeead20449ad2
Stuart A. Malone <samalone@llamagraphics.com>
2018-12-05 10:50:47 -0500

    Publish to Dropbox instead of web server

--- jose-jwt/nuget-push.ps1
+++ jose-jwt/nuget-push.ps1
@@ -1,4 +1,7 @@
 ﻿cd $PSScriptRoot
-$apiKey = Get-Content "\\Mac\Dropbox for Business\Business Dropbox for Antigen Plus\nuget-api-key.txt"
+#$apiKey = Get-Content "\\Mac\Dropbox for Business\Business Dropbox for Antigen Plus\nuget-api-key.txt"
 $package = gci *.nupkg | sort LastWriteTime | select -Last 1
-..\tools\nuget.exe push $package $apiKey -Source http://antigenplusnuget.azurewebsites.net/nuget
+#..\tools\nuget.exe push $package $apiKey -Source http://antigenplusnuget.azurewebsites.net/nuget
+
+$packageDir = "\\Mac\Dropbox for Business\Business Dropbox for Antigen Plus\Antigen Plus\packages"
+..\tools\nuget.exe add $package -source $packageDir
